ed5646eb706ebd3f38c48fb00a72c6d67dffff9d
Raj Singh <terahero554@gmail.com>
2023-08-13 21:44:12 +0530

    ..

--- lib/themes.dart
+++ lib/themes.dart
@@ -1,42 +1,21 @@
 // create a custom theme for light and dark theme
-
+//
 import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
-    primarySwatch: Colors.green,
-    appBarTheme: AppBarTheme(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      iconTheme: IconThemeData(
-        color: Colors.black,
-      ),
-    ),
+    primarySwatch: Colors.blue,
+    brightness: Brightness.light,
     scaffoldBackgroundColor: Colors.white,
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    appBarTheme: const AppBarTheme(
       backgroundColor: Colors.white,
       elevation: 0,
-      selectedItemColor: Colors.green,
-      unselectedItemColor: Colors.grey,
-    ),
-  );
-
-  static ThemeData darkTheme = ThemeData(
-    primarySwatch: Colors.green,
-    appBarTheme: AppBarTheme(
-      backgroundColor: Colors.black,
-      elevation: 0,
-      iconTheme: IconThemeData(
-        color: Colors.white,
+      iconTheme: IconThemeData(color: Colors.black),
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
       ),
-    ),
-    scaffoldBackgroundColor: Colors.black,
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: Colors.black,
-      elevation: 0,
-      selectedItemColor: Colors.green,
-      unselectedItemColor: Colors.grey,
     ),
   );
 }
-
